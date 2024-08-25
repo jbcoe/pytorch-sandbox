153bb51c96aacbebf0439362296b2dc07807f20a
Jonathan B. Coe <jonathanbcoe@gmail.com>
2024-08-25 20:05:17 +0100

    Play with data pipes (even though they are deprecated)

---
+++ data/data_loading.py
@@ -0,0 +1,85 @@
+"""Play with DataLoaders and DataPipes from pytorch."""
+
+import enum
+import logging
+import os
+from dataclasses import dataclass
+from subprocess import call
+
+import tyro
+from mnist_data import to_ascii_art
+from torchdata.datapipes.iter import IterableWrapper
+from torchvision import datasets, transforms  # type: ignore
+
+_LOGGER = logging.getLogger(__name__)
+
+
+class LogLevel(enum.IntEnum):
+    """Log levels for the logger as an enum."""
+
+    DEBUG = logging.DEBUG
+    INFO = logging.INFO
+    WARNING = logging.WARNING
+    ERROR = logging.ERROR
+    CRITICAL = logging.CRITICAL
+
+
+@dataclass
+class Config:
+    """Configuration for main."""
+
+    log_level: LogLevel = LogLevel.INFO
+    data_dir: str = ".DATA"
+
+
+class IsValue:
+    """Filter data by a specific value. Avoids lambda functions."""
+
+    def __init__(self, value):
+        """Initialize the filter."""
+        self.value = value
+
+    def __call__(self, x):
+        """Return True if the value is the same as the filter value."""
+        return x[1] == self.value
+
+
+class IsPrime:
+    """Filter data. Avoids lambda functions."""
+
+    def __call__(self, x):
+        """Return True if the value is prime."""
+        assert x[1] >= 0 and x[1] < 10, f"Value {x[1]} must be between in [0, 10)."
+        return x[1] in (2, 3, 5, 7)
+
+
+def main(config: Config):
+    """Main function for the demo."""
+    config = tyro.cli(Config)
+    logging.basicConfig(level=config.log_level)
+
+    transform = transforms.Compose(
+        [
+            transforms.ToTensor(),
+            transforms.Normalize(mean=(0.1307,), std=(0.3081,)),
+        ]
+    )
+    train = datasets.MNIST(
+        config.data_dir,
+        train=True,
+        download=True,
+        transform=transform,
+    )
+
+    datapipes = IterableWrapper(train).fork(2)
+
+    for data, _ in datapipes[0].filter(IsPrime()).repeat(2):
+        call("clear" if os.name == "posix" else "cls")
+        print(to_ascii_art(data[0]), "\n")
+        input()
+
+
+if __name__ == "__main__":
+    config = tyro.cli(Config)
+    logging.basicConfig(level=logging.INFO)
+    raise SystemExit(main(config))
