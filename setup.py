59319ccdb0668454eac375151d70a338cc90a521
Jonathan B. Coe <jonathanbcoe@gmail.com>
2024-09-14 14:27:22 +0100

    Remove setup.py - pyproject.toml can be used instead

--- setup.py
+++
@@ -1,9 +0,0 @@
-"""Setup script for the package."""
-
-from setuptools import find_packages, setup
-
-setup(
-    name="pytorch_sandbox",
-    packages=find_packages(),
-    py_modules=["pytorch_sandbox"],
-)
