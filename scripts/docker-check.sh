5a9631c45383dbd8e13544189a47389241492166
Jonathan B. Coe <jonathanbcoe@gmail.com>
2025-03-29 16:00:29 +0000

    Set up a devcontainer +cleanup (#7)

---
+++ scripts/docker-check.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+# A script to check if the docker image is usable.
+
+set -eu -o pipefail
+
+# Build the docker image
+echo "Building the docker image..."
+CMD="docker build -t pytorch-sandbox -f .devcontainer/Dockerfile ."
+echo "Running: $CMD"
+eval "$CMD"
+
+# Run the docker image
+printf "\e[1;32mChecking if the docker image is usable. Exit the container with ^d.\e[0m\n"
+
+docker run -it pytorch-sandbox bash
